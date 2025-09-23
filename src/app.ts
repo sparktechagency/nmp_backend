cb4bd12d9cf849425697b17672c0d40362b59508
goni715 <halesgoni715149123@gmail.com>
2025-09-23 12:38:13 +0600

    update cors

--- src/app.ts
+++ src/app.ts
@@ -12,16 +12,26 @@ import bodyParser from "body-parser";
 const app: Application = express();
 
 
-app.use(cors() );
-// app.use(
-//   cors({
-//     //origin: process.env.ALLOWED_ORIGINS?.split(",") || ["http://localhost:3000"],
-//     origin: "*",
-//     credentials: true,
-//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-//     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
-//   }),
-// )
+
+app.use(
+    cors({
+        origin: [
+            "http://localhost:5173",
+            "http://localhost:5174",
+            "http://localhost:5175",
+            "http://localhost:3000",
+            "http://localhost:3001",
+            "http://localhost:3002",
+            "http://localhost:3003",
+            "http://16.171.171.18:4173",
+            "http://16.171.171.18:5173",
+            "http://16.171.171.18:3000",
+            "http://16.171.171.18:30001",
+            "http://10.10.20.24:3000"
+        ],
+        credentials: true,
+    }),
+);
 
 
 app.use(cookieParser())
