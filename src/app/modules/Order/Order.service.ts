e3301fe2d15402efb4cb3109cbc72dca7ba1c009
goni715 <halesgoni715149123@gmail.com>
2025-10-11 16:10:53 +0600

    update order service.ts

--- src/app/modules/Order/Order.service.ts
+++ src/app/modules/Order/Order.service.ts
@@ -19,6 +19,175 @@ import calculateShippingCost from '../../utils/calculateShippingCost';
 const stripe = new Stripe(config.stripe_secret_key as string);
 
 
+// const createOrderService = async (
+//   loginUserId: string,
+//   userEmail: string,
+//   payload: IShipping
+// ) => {
+
+
+//   const carts = await CartModel.aggregate([
+//     {
+//       $match: {
+//         userId: new ObjectId(loginUserId)
+//       }
+//     },
+//     {
+//       $project: {
+//         _id:0,
+//         userId: 0,
+//         createdAt:0,
+//         updatedAt:0
+//       }
+//     }
+//   ]);
+
+//   if(carts?.length===0){
+//     throw new ApiError(404, "No items in cart.")
+//   }
+  
+//   //count subTotal
+//   const subTotal = carts?.reduce((total, currentValue) => total + (currentValue.price * currentValue.quantity), 0);
+
+//   //count shipping cost
+//   const shippingCost = await calculateShippingCost(subTotal);
+
+//   //count total
+//   const total = Number(subTotal + shippingCost);
+
+//   const cartProducts = carts?.map((cv) => ({
+//     ...cv,
+//     total: Number(cv.price) * Number(cv.quantity)
+//   }))
+
+
+//   let products = []
+
+
+//   // check product availability during order
+//   for (let i = 0; i < cartProducts?.length; i++) {
+//     const product = await ProductModel.findById(cartProducts[i].productId);
+//     const availableQty = Number(product?.quantity);
+
+//     if (cartProducts[i].quantity > availableQty) {
+//       throw new ApiError(
+//         400,
+//         availableQty > 0
+//           ? `Sorry, only ${availableQty} unit(s) of '${product?.name}' are left in stock. Please update your order.`
+//           : `Sorry, "${product?.name}" is currently out of stock.`
+//       );
+//     }
+
+//     products.push({
+//       productId: cartProducts[i].productId,
+//       name: product?.name,
+//       price: product?.currentPrice,
+//       quantity: product?.quantity,
+//       total: Number(product?.currentPrice) * Number(cartProducts[i].quantity),
+//       image: product?.image
+//     })
+//   }
+  
+
+//   const lineItems = cartProducts?.map((product) => ({
+//     price_data: {
+//       currency: "usd",
+//       product_data: {
+//         name: product.name,
+//       },
+//       unit_amount: product.price * 100, // price in cents
+//     },
+//     quantity: product.quantity,
+//   }));
+
+
+//     // add shipping as one item for the order
+//   if (shippingCost > 0) {
+//     lineItems.push({
+//       price_data: {
+//         currency: "usd",
+//         product_data: {
+//           name: "Shipping Cost",
+//         },
+//         unit_amount: shippingCost * 100, // in cents
+//       },
+//       quantity: 1,
+//     });
+//   }
+
+//    //generate token
+//   const token = Math.floor(100000 + Math.random() * 900000);
+
+//   //generate transactionId
+//   const transactionId = generateTransactionId();
+  
+
+//      //transaction & rollback
+//     const session = await mongoose.startSession();
+  
+//     try {
+//       session.startTransaction();
+
+//       // update product sales in bulk
+//       //bulkWrite send one request to MongoDB:
+//       await ProductModel.bulkWrite(
+//         cartProducts.map(item => ({
+//           updateOne: {
+//             filter: { _id: item.productId },
+//             update: { $inc: { total_sold: item.quantity, quantity: -item.quantity } },
+//           }
+//         })),
+//         { session }
+//       );
+
+//       //delete from cart list
+//       await CartModel.deleteMany(
+//         { userId: new ObjectId(loginUserId) },
+//         { session }
+//       );
+  
+//       const order = await OrderModel.create([
+//         {
+//           userId: loginUserId,
+//           token,
+//           products: cartProducts,
+//           subTotal,
+//           shippingCost,
+//           total,
+//           transactionId,
+//           shipping: payload
+//         }
+//       ], {session});
+
+//       //create payment session
+//         const paymentSession = await stripe.checkout.sessions.create({
+//           payment_method_types: ["card"],
+//           line_items: lineItems,
+//           mode: "payment",
+//           metadata: {
+//             orderId: (order[0]._id).toString(),
+//             userId: loginUserId
+//           },
+//           customer_email: userEmail,
+//           client_reference_id: (order[0]._id).toString(),
+//           success_url: `${config.frontend_url}/order-confirmed?session_id={CHECKOUT_SESSION_ID}`,
+//           cancel_url: `${config.frontend_url}/cancel`,
+//         });
+  
+//       //transaction success
+//       await session.commitTransaction();
+//       await session.endSession();
+//       return {
+//         url: paymentSession.url
+//       };
+//     } catch (err: any) {
+//       await session.abortTransaction();
+//       await session.endSession();
+//       throw new Error(err);
+//     }
+
+// };
+
 const createOrderService = async (
   loginUserId: string,
   userEmail: string,
@@ -59,6 +228,7 @@ const createOrderService = async (
     ...cv,
     total: Number(cv.price) * Number(cv.quantity)
   }))
+
 
 
   // check product availability during order
