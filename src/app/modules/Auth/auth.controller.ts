556697d3a35468cc45528c9a4f6507ded9c5e315
goni715 <halesgoni715149123@gmail.com>
2025-10-25 10:54:49 +0600

    update auth controller

--- src/app/modules/Auth/auth.controller.ts
+++ src/app/modules/Auth/auth.controller.ts
@@ -17,11 +17,22 @@ const registerUser = catchAsync(async (req, res) => {
 
 const verifyEmail = catchAsync(async (req, res) => {
   const result = await verifyEmailService(req.body);
+  const { accessToken, refreshToken } = result;
+
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,  // Prevents client-side access to the cookie (more secure)
+    secure: config.node_env === "production", // Only use HTTPS in production
+    maxAge: 7 * 24 * 60 * 60 * 1000, // Expires in 7 day
+    sameSite: "strict", // Prevents CSRF attacks
+  });
+
   sendResponse(res, {
     statusCode: 200,
     success: true,
     message: "Email is verified successfully",
-    data: result
+    data: {
+      accessToken
+    }
   })
 });
 
